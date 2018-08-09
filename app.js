6292a826333f81b6d685046bd1c2dd5003fba566
Joe Marchesi <joseph.a.marchesi@gmail.com>
2018-08-09 11:58:14 -0400

    Remove ENV

--- app.js
+++ app.js
@@ -36,12 +36,6 @@ let io = require('socket.io')(server)
 var middleware = require('socketio-wildcard')()
 io.use(middleware)
 
-// ENV
-require('dotenv').config()
-
-// Heroku API
-console.log(process.env.TEST_VAR)
-
 ////////////////////////////////////////////////////////////////////////////
 
 // Codenames Game
