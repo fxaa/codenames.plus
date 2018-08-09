83512b6cc568377ef259e07757f66d40ba3e22e2
Joe Marchesi <joseph.a.marchesi@gmail.com>
2018-08-09 16:05:27 -0400

    Troubleshoot restart

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ io.use(middleware)
 // Make API requests
 const Heroku = require('heroku-client')
 const heroku = new Heroku({ token: process.env.API_TOKEN })// DELETE requests
-heroku.delete('/apps/codenames-plus/dynos/').then(app => {})
+heroku.delete('/apps/codenames-plus/dynos/').then(app => {console.log(app[body])})
 
 // Daily Server Restart time
 // UTC 13:00:00 = 9AM EST
